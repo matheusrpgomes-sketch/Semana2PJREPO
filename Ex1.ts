53b4ee2d9f160bc51c3c9c136913455f26a08858
matheusrpgomes-sketch <matheusrpgomes@gmail.com>
2026-05-13 16:23:30 -0300

    feat:Ex1.ts

---
+++ Ex1.ts
@@ -0,0 +1,18 @@
+type User = {
+    name: string
+    age: number
+    active: boolean
+}
+const users: User[] = [
+  { name: 'Ana', age: 17, active: true },
+  { name: 'Diego', age: 25, active: true },
+  { name: 'Carla', age: 22, active: true },
+  { name: 'Bruno', age: 25, active: false },
+  { name: 'Eva', age: 15, active: true },
+]
+
+const UserFilter = users.filter(item => item.active === true && item.age >= 18);
+console.log(UserFilter);
+
+export const UserSort = UserFilter.sort((a, b) => a.name.localeCompare(b.name));
+console.log(UserSort);
